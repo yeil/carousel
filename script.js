5bbe5d4a3050791f529388c2f004e95cd4f1dd18
yeil <spideymuse@gmail.com>
2016-10-27 17:48:49 -0700

    refactor html and js

--- script.js
+++ script.js
@@ -1,67 +1,22 @@
 var leftButton = document.getElementsByClassName("left-button")[0];
 var rightButton = document.getElementsByClassName("right-button")[0];
 
-var blue = document.getElementsByClassName("group1")[0];
-var red = document.getElementsByClassName("group2")[0];
-var green = document.getElementsByClassName("group3")[0];
+var parentSlide = document.getElementsByClassName("carousel-slides")[0];
+var slides = document.getElementsByClassName("carousel-slide");
 
-var leftCount = 1;
-var rightCount = 1;
+function move(left) {
 
-function left() {
-  
-  if (leftCount === 1) {
-    blue.style.display = "none";
-    red.style.display = "none";
-    green.style.display = "inline-block";
-    rightCount += 2;
-    return leftCount++;
-  }
-    
-  if (leftCount === 2) {
-    green.style.display = "none";
-    blue.style.display = "none";
-    red.style.display = "inline-block";
-    rightCount--;
-    return leftCount++;
+  if(left) {
+    parentSlide.insertBefore(parentSlide.lastElementChild, parentSlide.firstElementChild);
+    parentSlide.firstElementChild.classList.add('visible');
+    slides[8].classList.remove('visible');
+  } else {
+    slides[8].classList.add('visible');
+    parentSlide.firstElementChild.classList.remove('visible');
+    parentSlide.appendChild(parentSlide.firstElementChild);
   }
 
-  if (leftCount === 3) {
-    red.style.display = "none";
-    green.style.display = "none";
-    blue.style.display = "inline-block";
-    rightCount--;
-    return leftCount -= 2;
-  }
 };
 
-leftButton.addEventListener("click", left);
-
-function right() {
-  
-  if (rightCount === 1) {
-    blue.style.display = "none";
-    green.style.display = "none";
-    red.style.display = "inline-block";
-    leftCount += 2;
-    return rightCount++;
-  }
-    
-  if (rightCount === 2) {
-    red.style.display = "none";
-    blue.style.display = "none";
-    green.style.display = "inline-block";
-    leftCount--;
-    return rightCount++;
-  }
-
-  if (rightCount === 3) {
-    green.style.display = "none";
-    red.style.display = "none";
-    blue.style.display = "inline-block";
-    leftCount--;
-    return rightCount -= 2;
-  }
-};
-
-rightButton.addEventListener("click", right);
+leftButton.addEventListener("click", function(){move(true)});
+rightButton.addEventListener("click", function(){move(false)});
